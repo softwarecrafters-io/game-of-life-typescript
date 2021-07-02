77ec3bb53985b360bb85a382ba7f2792e6bbb3c4
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-07-02 11:34:28 +0100

    green: any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction

--- src/tests/cell.test.ts
+++ src/tests/cell.test.ts
@@ -20,6 +20,9 @@ class Cell {
 				return CellStatus.Dead;
 			}
 		} else {
+			if (numberOfNeighbours === 3) {
+				return CellStatus.Alive;
+			}
 			return CellStatus.Dead;
 		}
 	}
@@ -38,5 +41,8 @@ describe('In the game of life', () => {
 	it('any live cell with more than three live neighbours dies, as if by overpopulation', () => {
 		expect(new Cell(CellStatus.Alive).regenerate(4)).toBe(CellStatus.Dead);
 		expect(new Cell(CellStatus.Dead).regenerate(4)).toBe(CellStatus.Dead);
+	});
+	it('any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction', () => {
+		expect(new Cell(CellStatus.Dead).regenerate(3)).toBe(CellStatus.Alive);
 	});
 });
