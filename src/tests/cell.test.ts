e25ad512cb5ebdaa96c2425da164600d5e1ffffb
Miguel A. Gomez <miguel.gomez@aderant.com>
2021-07-02 11:33:19 +0100

    green: any live cell with more than three live neighbours dies, as if by overpopulation

--- src/tests/cell.test.ts
+++ src/tests/cell.test.ts
@@ -35,4 +35,8 @@ describe('In the game of life', () => {
 		expect(new Cell(CellStatus.Alive).regenerate(3)).toBe(CellStatus.Alive);
 		expect(new Cell(CellStatus.Dead).regenerate(2)).toBe(CellStatus.Dead);
 	});
+	it('any live cell with more than three live neighbours dies, as if by overpopulation', () => {
+		expect(new Cell(CellStatus.Alive).regenerate(4)).toBe(CellStatus.Dead);
+		expect(new Cell(CellStatus.Dead).regenerate(4)).toBe(CellStatus.Dead);
+	});
 });
